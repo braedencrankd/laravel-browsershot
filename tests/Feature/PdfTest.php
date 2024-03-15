d11bb9604d90ed9b013d946880e2789952c4750e
braedenf <braedenleefoster@gmail.com>
2024-03-15 15:58:58 +1300

    Add KasmVNC and Playwright configurations

--- tests/Feature/PdfTest.php
+++ tests/Feature/PdfTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Invoice;
+use Spatie\Browsershot\Browsershot;
 use Spatie\LaravelPdf\PdfBuilder;
 use Spatie\LaravelPdf\Facades\Pdf;
 
@@ -8,7 +9,13 @@ it('can generate pdf', function () {
 
   $invoice = Invoice::factory()->create();
 
+
   $pdf = Pdf::view('pdfs.invoice', ['invoice' => $invoice])
+    ->withBrowsershot(function (Browsershot $browsershot) {
+      $browsershot->setNodeBinary('/usr/bin/node');
+      $browsershot->setNpmBinary('/usr/bin/npm');
+      $browsershot->noSandbox();
+    })
     ->format('a4')
     ->save('invoice.pdf');
 
